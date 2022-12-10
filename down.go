3f10aad3b468d5e36d56f8ba512c35a774e17fa2
fooofei <aihujianfei@qq.com>
2022-12-10 22:13:03 +0800

    add download to disk file

--- down.go
+++ down.go
@@ -7,6 +7,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"net/http"
+	"os"
 
 	"golang.org/x/sync/errgroup"
 )
@@ -25,19 +26,24 @@ func Do(ctx context.Context, clt Requester, url string) ([]byte, error) {
 
 	var concurrentCount = 48
 	var buf = make([]byte, totalSize, totalSize)
-	var taskList = makeTask(totalSize, buf)
-	var taskCh = make(chan Task, len(taskList))
+	var taskList = makeMemoryTask(totalSize, buf)
+	var taskCh = make(chan memoryTaskType, len(taskList))
 	for _, task := range taskList {
 		taskCh <- task
 	}
 	close(taskCh)
 
-	var group, _ = errgroup.WithContext(ctx)
+	var group, errCtx = errgroup.WithContext(ctx)
 
 	for i := 0; i < concurrentCount; i++ {
 		group.Go(func() error {
 			for task := range taskCh {
-				if err := readChunk(clt, req, task); err != nil {
+				select {
+				case <-errCtx.Done():
+					return nil
+				default:
+				}
+				if err := readChunk(clt, req.WithContext(errCtx), task); err != nil {
 					return err
 				}
 			}
@@ -61,6 +67,97 @@ func DoWithCheck(ctx context.Context, clt Requester, url, sha256Sum string) ([]b
 	return result, nil
 }
 
+func DoToFile(ctx context.Context, clt Requester, url, filePath string) error {
+	var req, err = http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return err
+	}
+	var preRead *HTTPReaderAt
+	if preRead, err = New(clt, req); err != nil {
+		return err
+	}
+	var totalSize = preRead.Size()
+	var taskCh = makeFileTask(totalSize)
+	var chunkResultCh = make(chan memoryTaskType, len(taskCh))
+
+	var file *os.File
+	if file, err = os.Create(filePath); err != nil {
+		return err
+	}
+	var concurrentCount = 48
+
+	var group, errCtx = errgroup.WithContext(ctx)
+
+	for i := 0; i < concurrentCount; i++ {
+		group.Go(func() error {
+			for task := range taskCh {
+				select {
+				case <-errCtx.Done():
+					return nil
+				default:
+				}
+				var mt = memoryTaskType{
+					Offset:  task.Offset,
+					Content: make([]byte, task.Size),
+				}
+				if err := readChunk(clt, req, mt); err != nil {
+					return err
+				}
+				select {
+				case <-errCtx.Done():
+					return nil
+				case chunkResultCh <- mt:
+				}
+			}
+			return nil
+		})
+	}
+
+	// single routine for write file
+	group.Go(func() error {
+		var totalWrite int64
+		for {
+			select {
+			case <-errCtx.Done():
+				return nil
+			case chunk := <-chunkResultCh:
+				if _, err := file.WriteAt(chunk.Content, chunk.Offset); err != nil {
+					return err
+				}
+				totalWrite += int64(len(chunk.Content))
+				if totalWrite == totalSize {
+					return nil
+				}
+			}
+		}
+	})
+	return group.Wait()
+}
+
+func makeFileTask(totalSize int64) <-chan fileTaskType {
+	const chunkSize int64 = 64 * 1024
+	var taskCount = totalSize / chunkSize
+	var taskList = make([]fileTaskType, taskCount)
+	var offset int64 = 0
+	for i := int64(0); i < taskCount; i++ {
+		taskList[i].Offset = offset
+		taskList[i].Size = chunkSize
+		offset += chunkSize
+	}
+	if offset < totalSize {
+		taskList = append(taskList, fileTaskType{
+			Offset: offset,
+			Size:   totalSize - offset,
+		})
+	}
+	var taskCh = make(chan fileTaskType, len(taskList))
+	for _, e := range taskList {
+		taskCh <- e
+	}
+	close(taskCh)
+	return taskCh
+}
+
 func equal(content []byte, checksum string) (bool, error) {
 	expect, err := hex.DecodeString(checksum)
 	if err != nil {
@@ -70,7 +167,7 @@ func equal(content []byte, checksum string) (bool, error) {
 	return hmac.Equal(v1[:], expect), nil
 }
 
-func readChunk(clt Requester, req *http.Request, task Task) error {
+func readChunk(clt Requester, req *http.Request, task memoryTaskType) error {
 	var chunkReader, err = New(clt, req)
 	if err != nil {
 		return err
@@ -86,12 +183,12 @@ func readChunk(clt Requester, req *http.Request, task Task) error {
 	return nil
 }
 
-func makeTask(totalSize int64, buf []byte) []Task {
+func makeMemoryTask(totalSize int64, buf []byte) []memoryTaskType {
 	var chunkSize int64 = 64 * 1024
-	var taskList []Task
+	var taskList []memoryTaskType
 
 	var taskCount = totalSize / chunkSize
-	taskList = make([]Task, taskCount, taskCount+1)
+	taskList = make([]memoryTaskType, taskCount, taskCount+1)
 	var offset int64 = 0
 	for i := int64(0); i < taskCount; i++ {
 		taskList[i].Offset = offset
@@ -99,7 +196,7 @@ func makeTask(totalSize int64, buf []byte) []Task {
 		offset += chunkSize
 	}
 	if offset < totalSize {
-		taskList = append(taskList, Task{
+		taskList = append(taskList, memoryTaskType{
 			Offset:  offset,
 			Content: buf[offset:totalSize],
 		})
@@ -107,7 +204,12 @@ func makeTask(totalSize int64, buf []byte) []Task {
 	return taskList
 }
 
-type Task struct {
+type memoryTaskType struct {
 	Offset  int64
 	Content []byte
+}
+
+type fileTaskType struct {
+	Offset int64
+	Size   int64
 }
